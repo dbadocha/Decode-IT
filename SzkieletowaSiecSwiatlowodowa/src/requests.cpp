a91b27dea2fc42d67961997002f376d93dc4b7ee
dbadocha <dawidbadocha@gmail.com>
2020-08-08 11:51:27 +0200

    changed from reading iostream chars to getline

--- SzkieletowaSiecSwiatlowodowa/src/requests.cpp
+++ SzkieletowaSiecSwiatlowodowa/src/requests.cpp
@@ -7,23 +7,41 @@ std::vector<Request> collectRequests()
 {
 	std::vector<Request> ret = {};
 	Request req = {};
-	char type = {};
+	std::string reqLine = {};
 
-	while (std::cin >> type)
+	while(std::getline(std::cin, reqLine))
 	{
-		req = {};
-		req.reqType = type;
-		req.ip1 = std::move(getIP());
-		req.ip2 = std::move(getIP());
-		ret.push_back(req);
+		ret.push_back(reqLineToRequest(reqLine));
 	}
 
 	return ret;
 }
 
-std::vector<int> getIP()
+std::vector<std::string> splitRequestLine(std::string reqLine)
 {
-	std::string ip = {};
-	std::cin >> ip;
-	return stringToIntIP(ip);
+	size_t start = 0, end = 0;
+	std::vector<std::string> ret;
+
+	while (end != std::string::npos)
+	{
+		end = reqLine.find_first_of(" \n\t\0", start);
+		ret.push_back(reqLine.substr(start, end - start));
+		start = end + 1;
+	}
+	return ret;
+}
+
+Request reqLineToRequest(std::string reqLine)
+{
+	std::vector<std::string> tmpReq = {};
+	tmpReq = std::move(splitRequestLine(reqLine));
+
+	Request req = {};
+	if (tmpReq.size() != 3)
+		return req;
+
+	req.reqType = tmpReq[0].c_str()[0];
+	req.ip1 = stringToIntIP(tmpReq[1]);
+	req.ip2 = stringToIntIP(tmpReq[2]);
+	return req;
 }
